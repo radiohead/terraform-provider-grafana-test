3a3d312602f2adec7c1adb56825665691f887e24
Igor Suleymanov <igor.suleymanov@grafana.com>
2025-04-03 16:26:50 +0300

    Prepare a more generic demo setup
    Signed-off-by: Igor Suleymanov <igor.suleymanov@grafana.com>

--- dashboards.tf
+++ dashboards.tf
@@ -1,32 +1,42 @@
-resource "grafana_apps_dashboard_dashboard_v1alpha1" "test_dashboard_one" {
-  metadata {
-    uid        = "test_dashboard_one"
-    folder_uid = grafana_folder.test_folder_one.uid
-  }
+# Current dashboard resource
+resource "grafana_dashboard" "test_dashboard_one" {
+  # Unlike in the new dashboard resource, one cannot specify the uid.
+  # uid         = "test_dashboard_one"
 
-  spec {
-    json  = file("${path.module}/dashboards/test_dashboard_one.json")
-    title = "Test Dashboard One"
-    tags = [
-      "one",
-      "two",
-      "three",
-    ]
-  }
+  # Also, the title cannot be specified in Terraform
+  # But it's required, so it must be present in the JSON file.
+  # title       = "Test Dashboard One"
 
-  options {
-    overwrite = true
-  }
+  folder      = grafana_folder.test_folder_one.uid
+  config_json = file("${path.module}/dashboards/test_dashboard_one.json")
+  overwrite   = true
 }
 
+# New dashboard resource
 resource "grafana_apps_dashboard_dashboard_v1alpha1" "test_dashboard_two" {
   metadata {
+    # In the new dashboard resource, the UID is required.
+    # This makes it easier to use deterministic (e.g. human-readable) UIDs.
+    # If the user doesn't care about the UID they can use a random UUID / string from Terraform.
     uid        = "test_dashboard_two"
     folder_uid = grafana_folder.test_folder_two.uid
   }
 
   spec {
+    # Dashboard JSON
     json = file("${path.module}/dashboards/test_dashboard_two.json")
+
+    # [OPTIONAL] Customize the title
+    # In the new dashboard resource, the title can be specified in Terraform
+    # (but it will be derived from the JSON file if not specified).
+    title = "Test Dashboard Two"
+
+    # [OPTIONAL] The tags can also be customized in Terraform.
+    tags = [
+      "one",
+      "two",
+      "three",
+    ]
   }
 
   options {
--- playlists.tf
+++ playlists.tf
@@ -1,6 +1,13 @@
+# New playlist resource
+# Follows the same format as the dashboard resource
 resource "grafana_apps_playlist_playlist_v0alpha1" "test_playlist" {
   metadata {
     uid = "test_playlist"
+
+    # Storing playlists in folders is not supported yet
+    # but it will be supported in the near future.
+    #
+    # folder_uid = grafana_folder.test_folder_one.uid
   }
 
   spec {
@@ -9,7 +16,7 @@ resource "grafana_apps_playlist_playlist_v0alpha1" "test_playlist" {
     items = [
       {
         type  = "dashboard_by_uid"
-        value = grafana_apps_dashboard_dashboard_v1alpha1.test_dashboard_one.metadata.uid
+        value = grafana_dashboard.test_dashboard_one.uid
       },
       {
         type  = "dashboard_by_uid"
